91e5ca5c8fdff9de51488c3e291444dc2cc5d116
Tatiana <t.podkur17@gmail.com>
2024-10-01 16:11:04 +0300

    add layout and styles for Movie Card

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import './app.css';
+import Card from './card/card';
+
+function App() {
+  return (
+    <div>
+      <ul className="cards-list">
+        <li>
+          <Card />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './card.css';
+
+const Card = () => {
+
+  const charactersPerLine = 60;
+  const linesCount = 7;
+  const originalText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quidem rerum sequi! Cumque deleniti earum harum recusandae voluptates. Animi deserunt eius, et expedita fugit officia sint voluptate? Consequatur, est, sint? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quidem rerum sequi! Cumque deleniti earum harum recusandae voluptates. Animi deserunt eius, et expedita fugit officia sint voluptate? Consequatur, est, sint? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quidem rerum sequi! Cumque deleniti earum harum recusandae voluptates. Animi deserunt eius, et expedita fugit officia sint voluptate? Consequatur, est, sint?';
+  const slicedTextInArr = originalText.slice(0, charactersPerLine * linesCount).split(' ');
+  slicedTextInArr.pop();
+  const displayedText = slicedTextInArr.join(' ');
+
+  return (
+    <div className="card">
+      <img className="card__image" alt="movie-poster"/>
+      <div className="card__info movie">
+        <h2 className="movie__title">The way back</h2>
+        <p className="movie__date">March 5, 2020</p>
+        <ul className="movie__genres-list">
+          <li className="movie__genre">
+            <div className="genre">Action</div>
+          </li>
+          <li className="movie__genre">
+            <div className="genre">Drama</div>
+          </li>
+        </ul>
+        <p className="movie__description">{displayedText + ' ...'}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
