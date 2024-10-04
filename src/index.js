7d8ee0329fcc5c419cf3907d18a25349d73c6329
Tatiana <t.podkur17@gmail.com>
2024-10-04 16:17:30 +0300

    handle situation when no network

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
+import { Alert } from 'antd';
+import { Offline, Online } from 'react-detect-offline';
+
 import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Online>
+      <App />
+    </Online>
+    <Offline>
+      <div className="offline-error">
+        <Alert type="error" message={'There is no internet connection.'} />
+      </div>
+    </Offline>
   </React.StrictMode>
 );
