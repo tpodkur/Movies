a4f18a951a4937e0e44eca640906aaaeaec08711
Tatiana <t.podkur17@gmail.com>
2024-10-07 09:35:15 +0300

    add messages for empty query and no results

--- src/components/cards-list/cards-list.js
+++ src/components/cards-list/cards-list.js
@@ -52,6 +52,7 @@ export default class CardsList extends Component {
         />
       </div>
     );
+
     const content = (
       <ul className="cards-list">
         {this.state.movies.map((movie) => (
@@ -59,6 +60,15 @@ export default class CardsList extends Component {
             <Card {...movie} genres={this.genres} loading={this.state.loading} error={this.state.error} />
           </li>
         ))}
+
+        {this.props.searchValue.length === 0 && (
+          <p className="cards-list__message">Please enter a query to search for movies ...</p>
+        )}
+        {this.props.searchValue.length !== 0 && this.state.movies.length === 0 && (
+          <p className="cards-list__message">
+            Sorry! Looks like we don&#39;t have any matches for &#34;{this.props.searchValue}&#34;
+          </p>
+        )}
       </ul>
     );
 
