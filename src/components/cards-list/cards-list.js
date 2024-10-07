27d9b5e13e6926c89cca52d4edd171f52dd14795
Tatiana <t.podkur17@gmail.com>
2024-10-07 13:09:35 +0300

    make cards-list functional component

--- src/components/cards-list/cards-list.js
+++ src/components/cards-list/cards-list.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Card from '../card/card';
 import './cards-list.scss';
 
-export default class CardsList extends Component {
-  render() {
-    const { movies, genres, loading, searchValue } = this.props;
+const CardsList = ({ movies, genres, loading, searchValue }) => {
+  return (
+    <ul className="cards-list">
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Card {...movie} genres={genres} loading={loading} />
+        </li>
+      ))}
 
-    return (
-      <ul className="cards-list">
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Card {...movie} genres={genres} loading={loading} />
-          </li>
-        ))}
+      {searchValue.length === 0 && <p className="cards-list__message">Please enter a query to search for movies ...</p>}
+      {searchValue.length !== 0 && movies.length === 0 && (
+        <p className="cards-list__message">
+          Sorry! Looks like we don&#39;t have any matches for &#34;{searchValue}&#34;
+        </p>
+      )}
+    </ul>
+  );
+};
 
-        {searchValue.length === 0 && (
-          <p className="cards-list__message">Please enter a query to search for movies ...</p>
-        )}
-        {searchValue.length !== 0 && movies.length === 0 && (
-          <p className="cards-list__message">
-            Sorry! Looks like we don&#39;t have any matches for &#34;{searchValue}&#34;
-          </p>
-        )}
-      </ul>
-    );
-  }
-}
+export default CardsList;
