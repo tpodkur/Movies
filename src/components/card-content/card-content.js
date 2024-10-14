9a68e040e156a822a21225d9457f23f88507baa8
Tatiana <t.podkur17@gmail.com>
2024-10-14 13:04:50 +0700

    add vote average to card

--- src/components/card-content/card-content.js
+++ src/components/card-content/card-content.js
@@ -33,6 +33,13 @@ const convertGenres = (genreIds, genreNamesArr) => {
 const convertReleaseDate = (date) => (date ? format(new Date(date), 'MMMM d, y') : null);
 
 export default class CardContent extends Component {
+  voteAverageElement;
+
+  constructor() {
+    super();
+    this.voteAverageElement = React.createRef();
+  }
+
   onChangeRating = (value) => {
     this.props.api.postRating(this.props.movie.id, value).then((res) => {
       // if (res.success) {
@@ -42,6 +49,21 @@ export default class CardContent extends Component {
     });
   };
 
+  componentDidMount() {
+    this.voteAverageElement.current.style.borderColor = this.getVoteAverageColor(this.props.movie.voteAverage);
+  }
+
+  getVoteAverageColor(value) {
+    if (value < 3) {
+      return '#E90000';
+    } else if (value < 5) {
+      return '#E97E00';
+    } else if (value < 7) {
+      return '#E9D100';
+    }
+    return '#66E900';
+  }
+
   render() {
     const { genres, movie } = this.props;
 
@@ -50,12 +72,18 @@ export default class CardContent extends Component {
     const movieDescription = convertDescription(movie.description);
     const movieGenres = convertGenres(movie.genreIds, genres);
     const movieReleaseDate = convertReleaseDate(movie.releaseDate);
+    const movieVoteAverage = movie.voteAverage.toFixed(1);
 
     return (
       <>
         <img className="card__image" alt="movie-poster" src={movieImgPath} />
         <div className="card__info movie">
-          <h2 className="movie__name">{movieName}</h2>
+          <div className="card__header">
+            <h2 className="movie__name">{movieName}</h2>
+            <span ref={this.voteAverageElement} className="movie__vote-average">
+              {movieVoteAverage}
+            </span>
+          </div>
           <p className="movie__date">{movieReleaseDate}</p>
           <ul className="movie__genres-list">{movieGenres}</ul>
           <p className="movie__description">{movieDescription}</p>
